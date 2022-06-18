93f1d4fd25211e9c8aa53fec90eda1c178c67bdc
Aviral Mehra <aviral.mehra008@gmail.com>
2022-06-18 14:00:49 +0530

    Implemented BFS and DFS in Graph

--- src/Graph/GraphUsingAdjacencyMatrix.java
+++ src/Graph/GraphUsingAdjacencyMatrix.java
@@ -1,5 +1,7 @@
 package Graph;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
 public class GraphUsingAdjacencyMatrix
 {
     ArrayList<GraphNode> nodeList=new ArrayList<GraphNode>();
@@ -34,6 +36,71 @@ public class GraphUsingAdjacencyMatrix
         }
         return s.toString();
     }
+      // get Neighbors
+  public ArrayList<GraphNode> getNeighbors(GraphNode node) {
+    ArrayList<GraphNode> neighbors = new ArrayList<GraphNode>();
+    int nodeIndex = node.index;
+    for (int i=0; i<adjacencymatrix.length; i++) {
+      if(adjacencymatrix[nodeIndex][i]==1) {
+        neighbors.add(nodeList.get(i));
+      }
+    }
+    return neighbors;
+
+  }
+
+  // BSF internall
+  void bfsVisit(GraphNode node) {
+    LinkedList<GraphNode> queue = new LinkedList<GraphNode>();
+    queue.add(node);
+    while(!queue.isEmpty()) {
+      GraphNode currentNode = queue.remove(0);
+      currentNode.isVisited = true;
+      System.out.print(currentNode.name + " ");
+      ArrayList<GraphNode> neighbors = getNeighbors(currentNode);
+      for (GraphNode neighbor: neighbors) {
+        if (!neighbor.isVisited) {
+          queue.add(neighbor);
+          neighbor.isVisited = true;
+        }
+      }
+    }
+  }
+
+  public void bfs() {
+    for (GraphNode node : nodeList) {
+      if(!node.isVisited) {
+        bfsVisit(node);
+      }
+    }
+  }
+
+  public void dfsVisit(GraphNode node)
+  {
+    Stack<GraphNode> stack = new Stack<>();
+    stack.push(node);
+    while(!stack.isEmpty()) {
+      GraphNode currentNode = stack.pop();
+      currentNode.isVisited = true;
+      System.out.print(currentNode.name + " ");
+      ArrayList<GraphNode> neighbors = getNeighbors(currentNode);
+      for (GraphNode neighbor : neighbors) {
+        if (!neighbor.isVisited) {
+          stack.push(neighbor);
+          neighbor.isVisited = true;
+        }
+      }
+
+    }
+  }
+
+  void dfs() {
+    for (GraphNode node : nodeList) {
+      if(!node.isVisited) {
+        dfsVisit(node);
+      }
+    }
+  }
     public static void main(String args[])
     {
         ArrayList<GraphNode> nodeList=new ArrayList<GraphNode>();
@@ -51,5 +118,6 @@ public class GraphUsingAdjacencyMatrix
         g.addUndirectedEdge(2, 3);
         g.addUndirectedEdge(3, 4);
         System.out.print(g.toString());
+        g.bfs();
     }
 }
